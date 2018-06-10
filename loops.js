1e6433157ac391ce72d861ff3ae48ac276f22129
Sarah Jacob <smjacob91@gmail.com>
2018-06-10 14:56:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,5 +19,18 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
   
+  do {
+    array.pop();
+    } while (array.length > 0 && maybeTrue());
+    return array;
 }
+
+
+
+
+
+
